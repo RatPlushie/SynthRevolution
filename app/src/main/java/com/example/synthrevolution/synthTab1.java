9b199a878358915524a6341b0bb6f8a70b9f4ee5
Aki <kieran.mortimer@gmail.com>
2020-02-08 18:49:13 +0100

    Colour picker assets

--- app/src/main/java/com/example/synthrevolution/synthTab1.java
+++ app/src/main/java/com/example/synthrevolution/synthTab1.java
@@ -1,5 +1,6 @@
 package com.example.synthrevolution;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,6 +8,7 @@ import android.view.ViewGroup;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import androidx.annotation.ColorInt;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -37,8 +39,8 @@ public class synthTab1 extends Fragment {
 
         totalLED_Brightness = view.findViewById(R.id.LEDBrightnessTotal);
         totalLED_Red        = view.findViewById(R.id.LEDRedTotal);
-        totalLED_Green      = view.findViewById(R.id.LEDBlueTotal);
-        totalLED_Blue       = view.findViewById(R.id.LEDGreenTotal);
+        totalLED_Green      = view.findViewById(R.id.LEDGreenTotal);
+        totalLED_Blue       = view.findViewById(R.id.LEDBlueTotal);
 
         colourView          = view.findViewById(R.id.colourView);
 
@@ -49,8 +51,8 @@ public class synthTab1 extends Fragment {
         // Populating TextViews onCreate
         totalLED_Brightness.setText(Integer.toString(mVisor.brightness));
         totalLED_Red.setText(Integer.toString(mVisor.RGB_Red));
-        totalLED_Blue.setText(Integer.toString(mVisor.RGB_Green));
-        totalLED_Green.setText(Integer.toString(mVisor.RGB_Blue));
+        totalLED_Blue.setText(Integer.toString(mVisor.RGB_Blue));
+        totalLED_Green.setText(Integer.toString(mVisor.RGB_Green));
 
 
         seekbarBrightness.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
@@ -58,7 +60,7 @@ public class synthTab1 extends Fragment {
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
 
                 // Storing the current value of the seekBar
-                mVisor.setBrightness(seekBar);
+                mVisor.setBrightness(progress);
 
                 // Displaying the progress bar numerically in the textView
                 totalLED_Brightness.setText(Integer.toString(progress));
@@ -82,10 +84,12 @@ public class synthTab1 extends Fragment {
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
 
                 // Storing the current value of the seekBar
-                mVisor.setRGB_Red(seekBar);
+                mVisor.setRGB_Red(progress);
 
                 // Displaying the progress bar numerically in the textView
                 totalLED_Red.setText(Integer.toString(progress));
+
+                colourView.setBackgroundColor();
             }
 
             @Override
@@ -106,10 +110,12 @@ public class synthTab1 extends Fragment {
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
 
                 // Storing the current value of the seekBar
-                mVisor.setRGB_Green(seekBar);
+                mVisor.setRGB_Green(progress);
 
                 // Displaying the progress bar numerically in the textView
                 totalLED_Green.setText(Integer.toString(progress));
+
+                colourView.setBackgroundColor();
             }
 
             @Override
@@ -130,10 +136,12 @@ public class synthTab1 extends Fragment {
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
 
                 // Storing the current value of the seekBar
-                mVisor.setRGB_Green(seekBar);
+                mVisor.setRGB_Blue(progress);
 
                 // Displaying the progress bar numerically in the textView
                 totalLED_Blue.setText(Integer.toString(progress));
+
+                colourView.setBackgroundColor();
             }
 
             @Override
@@ -162,29 +170,42 @@ public class synthTab1 extends Fragment {
         int RGB_Green;
         int RGB_Blue;
 
+        Color rgbColour;
+
+
         // Constructor method for onCreate
         VisorState(){
             brightness = 0;
             RGB_Red = 0;
             RGB_Green = 0;
             RGB_Blue = 0;
+
+            rgbColour = new Color(255, 255, 255);
+
+
+
+
         }
 
-        void setBrightness(SeekBar seekBar){
-            brightness = seekBar.getProgress();
+        void setBrightness(int progress){
+            brightness = progress;
         }
 
-        void setRGB_Red(SeekBar seekbar){
-            RGB_Red = seekbar.getProgress();
+        void setRGB_Red(int progress){
+            RGB_Red = progress;
         }
 
-        void setRGB_Green(SeekBar seekbar){
-            RGB_Green = seekbar.getProgress();
+        void setRGB_Green(int progress){
+            RGB_Green = progress;
         }
 
-        void setRGB_Blue(SeekBar seekbar){
-            RGB_Blue = seekbar.getProgress();
+        void setRGB_Blue(int progress){
+            RGB_Blue = progress;
         }
+
+
+
+
 
 
     }
