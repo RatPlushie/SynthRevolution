be2b6c92c2807a5471966b6dfe710b7493b2807d
Aki <kieran.mortimer@gmail.com>
2020-02-08 20:08:36 +0100

    LED Brightness seekbar implemented

--- app/src/main/java/com/example/synthrevolution/synthTab1.java
+++ app/src/main/java/com/example/synthrevolution/synthTab1.java
@@ -9,6 +9,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -19,9 +20,12 @@ public class synthTab1 extends Fragment {
 
     ImageView   colourPickerWheel;
     TextView    colourPickerResults;
+    TextView    LEDBrightnessTotal;
     View        colourPickerSelected;
+    SeekBar     seekbarLEDBrightness;
 
-    Bitmap      bitmap;
+
+    Bitmap      colourBitmap;
 
     @SuppressLint("ClickableViewAccessibility")
     @Nullable
@@ -32,12 +36,14 @@ public class synthTab1 extends Fragment {
         colourPickerWheel       = view.findViewById(R.id.colourPickerImageView);
         colourPickerResults     = view.findViewById(R.id.colourPickerTextView);
         colourPickerSelected    = view.findViewById(R.id.colourView);
-
+        seekbarLEDBrightness    = view.findViewById(R.id.LEDBrightnessSeekBar);
+        LEDBrightnessTotal      = view.findViewById(R.id.totalLEDBrightnessTextView);
 
 
         // Set initial display of selected colour and hex/RGB string
         colourPickerSelected.setBackgroundColor(Color.rgb(255, 255, 255));
         colourPickerResults.setText("RGB: 255, 255, 255 \nHEX: #ffffff");
+        LEDBrightnessTotal.setText(Integer.toString(0));
 
         colourPickerWheel.setDrawingCacheEnabled(true);
         colourPickerWheel.buildDrawingCache(true);
@@ -48,9 +54,9 @@ public class synthTab1 extends Fragment {
             public boolean onTouch(View v, MotionEvent event) {
 
                 if (event.getAction() == MotionEvent.ACTION_DOWN || event.getAction() == MotionEvent.ACTION_MOVE){
-                    bitmap = colourPickerWheel.getDrawingCache();
+                    colourBitmap = colourPickerWheel.getDrawingCache();
 
-                    int pixel = bitmap.getPixel((int)event.getX(), (int)event.getY());
+                    int pixel = colourBitmap.getPixel((int)event.getX(), (int)event.getY());
 
                     // Getting the RGB values
                     int r = Color.red(pixel);
@@ -71,59 +77,28 @@ public class synthTab1 extends Fragment {
         });
 
 
+        seekbarLEDBrightness.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                LEDBrightnessTotal.setText(Integer.toString(progress));
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
 
         // Returns the view to the layout inflater
         return view;
     }
-
-
-
-/*
-    class VisorState {
-        int brightness;
-        int RGB_Red;
-        int RGB_Green;
-        int RGB_Blue;
-
-        Color rgbColour;
-
-
-        // Constructor method for onCreate
-        VisorState(){
-            brightness = 0;
-            RGB_Red = 0;
-            RGB_Green = 0;
-            RGB_Blue = 0;
-
-            rgbColour = new Color(255, 255, 255);
-
-
-
-
-        }
-
-        void setBrightness(int progress){
-            brightness = progress;
-        }
-
-        void setRGB_Red(int progress){
-            RGB_Red = progress;
-        }
-
-        void setRGB_Green(int progress){
-            RGB_Green = progress;
-        }
-
-        void setRGB_Blue(int progress){
-            RGB_Blue = progress;
-        }
-
-
-
-
-
-
-    }
-
- */
 }
