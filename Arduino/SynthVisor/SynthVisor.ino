67689805472855584f2bd1fbf23f5f4f95291bd8
Aki <kieran.mortimer@gmail.com>
2020-02-21 17:12:00 +0100

    RGB LED lights up
    Now, once the arduino receives the config, it will display the visor config

--- Arduino/SynthVisor/SynthVisor.ino
+++ Arduino/SynthVisor/SynthVisor.ino
@@ -1,14 +1,12 @@
 #include <SoftwareSerial.h>                                   // While prototyping, I got annoyed repeatedly pulling in and out the tx/rx pins to upload to arduino - moved serial to pins 2,3
 SoftwareSerial Bluetooth(2,3);                                // RX|TX (Arduino side)
 
-                                                              // Using 5 lEDs to test extrapolation untill I have access to an 8x8 led matrix
-const int redLED        = 9;                                  // Red LED
-const int greenLED      = 10;                                 // Green LED
-const int blueLED       = 11;                                 // Blue LED
-const int brightnessLED = 5;                                  // Brightness indicator LED
-const int blinkLED      = 6;                                  // Blink rate indicator LED
+const int rgb_red = 9;                                        // For prototyping purposes, the rgb matrix is substituted as a singular RGB LED
+const int rgb_green = 10;
+const int rgb_blue = 11;
+const int indicatorLED = 12;                                  // LED to display when bluetooth data is being received
 
-int red = 0;                        
+int red = 0;                                                  // Global Values for the visor
 int green = 0;
 int blue = 0;
 int brightness = 0;
@@ -18,23 +16,21 @@ void setup() {                                                // Put your setup 
   Serial.begin(9600);                                         // Baudrate for the PC serial monitor
   Bluetooth.begin(9600);                                      // Baudrate for the Bluetooth serial
 
-  pinMode(redLED, OUTPUT);   
-  pinMode(greenLED, OUTPUT);
-  pinMode(blueLED, OUTPUT);
-  pinMode(brightnessLED, OUTPUT);
-  pinMode(blinkLED, OUTPUT);
+  pinMode(rgb_red, OUTPUT);
+  pinMode(rgb_green, OUTPUT);
+  pinMode(rgb_blue, OUTPUT);
+  pinMode(indicatorLED, OUTPUT);
 
-  digitalWrite(redLED, LOW);
-  digitalWrite(greenLED, LOW);
-  digitalWrite(blueLED, LOW);
-  digitalWrite(brightnessLED, LOW);
-  digitalWrite(blinkLED, LOW);
+  digitalWrite(rgb_red, LOW);
+  digitalWrite(rgb_green, LOW);
+  digitalWrite(rgb_blue, LOW);
+  digitalWrite(indicatorLED, LOW);
 }
 
 
 void loop() {                                                 // Put your main code here, to run repeatedly
   
-  if (Bluetooth.available() > 0){                             // Retreiveing the bluetooth serial input
+  if (Bluetooth.available() > 0){                             // Retreiveing the bluetooth serial input and parsing it into the global variables
 
     char receivedArray[20];
     int  blueCounter = 0;
@@ -43,18 +39,12 @@ void loop() {                                                 // Put your main c
       receivedArray[blueCounter] = char(Bluetooth.read());    // Storing received byte into array               
       blueCounter++;                                          // Iterating the counter for the next loop
 
-      digitalWrite(redLED, HIGH);                             // Blink LED to show something being receieved
-      delay(5);
-      digitalWrite(redLED, LOW);
+      digitalWrite(indicatorLED, HIGH);                       // Blink LED to show something being receieved
+      delay(2);
+      digitalWrite(indicatorLED, LOW);
       
       delay(250);                                             // Delay to allow the bluetooth to catch up
     }
-
-    /*
-    for (char c : receivedArray){                             // DEBUG PRINT OUT
-      Serial.write(c);
-    }
-    */
 
     int colourCount = 0;
     int valCount    = 0;
@@ -74,22 +64,27 @@ void loop() {                                                 // Put your main c
         switch(colourCount){                                  // Inputing the final value into its correct visor config
           case 0:
             red = atoi(valArray);
+            //Serial.println(red);
             break;
 
           case 1:
             green = atoi(valArray);
+            //Serial.println(green);
             break;
 
           case 2:
             blue = atoi(valArray);
+            //Serial.println(blue);
             break;
 
           case 3:
             brightness = atoi(valArray);
+            //Serial.println(brightness);
             break;
 
           case 4:                                   
             blinkRate = atoi(valArray);
+            //Serial.println(blinkRate);
             break;
         }
 
@@ -98,9 +93,7 @@ void loop() {                                                 // Put your main c
       }  
     }
     
-    Serial.println("");                                       // DEBUG PRINTOUT
-    Serial.println("");
-    Serial.println("VisorConfig:");
+    Serial.println("VisorConfig:");                           // Serial Debug of parsed config
     Serial.print("Red: ");
     Serial.println(red);
     Serial.print("Green: ");
@@ -110,7 +103,13 @@ void loop() {                                                 // Put your main c
     Serial.print("Brightness: "); 
     Serial.println(brightness);
     Serial.print("Blink Rate: ");
-    Serial.println(blinkRate);
-   
+    Serial.println(blinkRate); 
+    Serial.println("");
+    
  }
+
+  analogWrite(rgb_red, red);                                  // Displaying the loaded colour to the LED
+  analogWrite(rgb_green, green);
+  analogWrite(rgb_blue, blue);
+ 
 }
