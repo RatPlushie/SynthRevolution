00ce9a36568e7e6044a67d1385579a7ec0df7e13
Aki <kieran.mortimer@gmail.com>
2020-02-28 22:53:35 +0100

    Migrating SynthPattern behaviour to class
    To make initial navigation to the third tab, the onCreate behaviour of the synth visor pattern will be moved to the main class so it is initialised on launch of the app and not on navigation

--- Android App/app/src/main/java/com/ratbox/synthrevolution/synthTab3.java
+++ Android App/app/src/main/java/com/ratbox/synthrevolution/synthTab3.java
@@ -1,8 +1,6 @@
 package com.ratbox.synthrevolution;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,24 +15,9 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.List;
+import com.ratbox.synthrevolution.ui.main.SynthPattern;
 
 public class synthTab3 extends Fragment {
-
-    private static final String FILENAME = "VisorPatternConfig.txt";
-
-    private List<String>        patternNameList = new ArrayList<>();
-    private ArrayList<char[]>   patternConfList = new ArrayList<>();
 
     private Spinner                 configSpinner;
     private ImageButton             btnAddNewPattern;
@@ -197,96 +180,13 @@ public class synthTab3 extends Fragment {
                                   chkbtn49, chkbtn50, chkbtn51, chkbtn52, chkbtn53, chkbtn54, chkbtn55, chkbtn56,
                                   chkbtn57, chkbtn58, chkbtn59, chkbtn60, chkbtn61, chkbtn62, chkbtn63, chkbtn64};
 
-        // Attempting to read the save file and parse its contents
-        BufferedReader reader = null;
-        try {
-            FileInputStream fileInputStream = getContext().openFileInput(FILENAME);
-            reader = new BufferedReader(new InputStreamReader(fileInputStream));
 
-            String readString;
-            while ((readString = reader.readLine()) != null){ // EoF Check
-                // Splitting the line into name and pattern
-                String[] splitString = readString.split("=");
-
-                char[] patternArray = splitString[1].toCharArray();
-
-                // Storing the name and pattern in the lists
-                patternNameList.add(splitString[0]);
-                patternConfList.add(patternArray);
-            }
-
-            Log.d("File Read", "Successful");
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            Log.d("PatternFile", "No file found, Creating default");
-
-            // No pattern save file was found on the device, creating a new save file and filling it with the default synth eye pattern
-            BufferedWriter writer = null;
-            try {
-                FileOutputStream fileOutputStream = getContext().openFileOutput(FILENAME, Context.MODE_PRIVATE);
-                writer = new BufferedWriter(new OutputStreamWriter(fileOutputStream));
-
-                // Creating the default pattern for the eye
-                char[] defaultPattern = {'0','0','0','0','0','0','0','0',
-                                         '0','0','1','1','1','1','1','0',
-                                         '0','1','1','1','1','1','1','1',
-                                         '1','1','0','1','1','1','1','1',
-                                         '1','1','0','1','1','1','1','0',
-                                         '1','1','0','1','1','1','0','0',
-                                         '0','1','0','1','1','0','0','0',
-                                         '0','0','0','0','0','0','0','0'};
-
-                // adding the default pattern array to the list of arrays
-                patternNameList.add("Default");
-                patternConfList.add(defaultPattern);
-
-                // StringBuilder init
-                StringBuilder stringBuilder = new StringBuilder();
-
-                // Building string to write
-                stringBuilder.append("Default=");
-                for (char c : defaultPattern){
-                    stringBuilder.append(c);
-                }
-
-                // Writing string to file
-                writer.write(stringBuilder.toString());
-
-            } catch (FileNotFoundException ex) {
-                ex.printStackTrace();
-
-            } catch (IOException ex) {
-                ex.printStackTrace();
-
-            } finally { // Closing the writer
-                if (writer != null){
-                    try {
-                        writer.close();
-
-                    } catch (IOException eIO) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-
-        } finally { // closing the reader
-            if (reader != null){
-                try {
-                    reader.close();
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
+        // Initialising synthPattern Object
+        SynthPattern synthPattern = new SynthPattern(getContext(), checkBoxArr);
 
 
         // Creating the array adapter, attaching it and setting the onClickListener
-        ArrayAdapter<String> patternAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, patternNameList);
+        ArrayAdapter<String> patternAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, synthPattern.patternNameList);
         patternAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         configSpinner.setAdapter(patternAdapter);
         configSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
@@ -304,7 +204,7 @@ public class synthTab3 extends Fragment {
         });
 
         // Loading the default eye pattern onCreate()
-        setPattern(checkBoxArr, patternConfList.get(0));
+        synthPattern.setPattern(0);
 
 
         // Setting the onclick listener for the "add new pattern" button
@@ -332,22 +232,5 @@ public class synthTab3 extends Fragment {
         return view;
     }
 
-    // Method for loading the saved pattern onto the checkboxes
-    private void setPattern(CheckBox[] checkBoxes, char[] pattern){
-        for (int i = 0; i < checkBoxes.length - 1; i++){
 
-            // Parsing the bit to a bool
-            Boolean bool;
-            if (pattern[i] == '1'){
-                bool = true;
-            } else {
-                bool = false;
-            }
-
-            // Setting the checkbox to either checked or unchecked
-            checkBoxes[i].setChecked(bool);
-        }
-
-        Log.d("CheckBoxes", "Set");
-    }
 }
